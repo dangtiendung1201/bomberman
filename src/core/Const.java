345dc84e40f0973243eec2dcc09c29be3f4883e1
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-08-18 09:01:34 +0700

    Add: Menu buttons effect

--- src/core/Const.java
+++ src/core/Const.java
@@ -1,9 +1,32 @@
 package core;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+
 public class Const {
     public static final int WIDTH = 1200;
     public static final int HEIGHT = 600;
     public static final String TITLE = "Bomberman";
+
+    public enum STATE {
+        MENU,
+        PLAYER,
+        COMPUTER,
+        EXIT
+    }
+
+    public static STATE gameState = STATE.MENU;
+
+    public static final int FPS = 60;
+
+    public static final ImageView menuImage = new ImageView(new Image("file:res/menu.png"));
 
     // Menu buttons
     public static final int BUTTON_WIDTH = 228;
@@ -11,12 +34,54 @@ public class Const {
 
     public static final int BUTTON_PLAYER_START_X = 486;
     public static final int BUTTON_PLAYER_START_Y = 340;
+    public static final Button buttonPlayer = new Button("");
 
     public static final int BUTTON_COMPUTER_START_X = 486;
     public static final int BUTTON_COMPUTER_START_Y = 432;
+    public static final Button buttonComputer = new Button("");
 
     public static final int BUTTON_EXIT_START_X = 486;
     public static final int BUTTON_EXIT_START_Y = 524;
+    public static final Button buttonExit = new Button("");
 
+    public static void loadMenu() {
+        buttonPlayer.setLayoutX(BUTTON_PLAYER_START_X);
+        buttonPlayer.setLayoutY(BUTTON_PLAYER_START_Y);
+        buttonPlayer.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+        buttonPlayer.setStyle("-fx-background-color: transparent;");
+        buttonPlayer.setOnMouseEntered(e -> {
+            buttonPlayer.setStyle("-fx-background-color: transparent; -fx-border-color: green; -fx-border-width: 2px;");
+        });
+        buttonPlayer.setOnMouseExited(e -> {
+            buttonPlayer.setStyle("-fx-background-color: transparent;");
+
+        });
+
+        buttonComputer.setLayoutX(BUTTON_COMPUTER_START_X);
+        buttonComputer.setLayoutY(BUTTON_COMPUTER_START_Y);
+        buttonComputer.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+        buttonComputer.setStyle("-fx-background-color: transparent;");
+        buttonComputer.setOnMouseEntered(e -> {
+            buttonComputer
+                    .setStyle("-fx-background-color: transparent; -fx-border-color: green; -fx-border-width: 2px;");
+        });
+        buttonComputer.setOnMouseExited(e -> {
+            buttonComputer.setStyle("-fx-background-color: transparent;");
+
+        });
+
+        buttonExit.setLayoutX(BUTTON_EXIT_START_X);
+        buttonExit.setLayoutY(BUTTON_EXIT_START_Y);
+        buttonExit.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+        buttonExit.setStyle("-fx-background-color: transparent;");
+        buttonExit.setOnMouseEntered(e -> {
+            buttonExit
+                    .setStyle("-fx-background-color: transparent; -fx-border-color: green; -fx-border-width: 2px;");
+        });
+        buttonExit.setOnMouseExited(e -> {
+            buttonExit.setStyle("-fx-background-color: transparent;");
+
+        });
+    }
 
 }
--- src/core/Game.java
+++ src/core/Game.java
@@ -2,10 +2,7 @@ package core;
 
 import static core.Const.*;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
+import core.Const.STATE;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -24,62 +21,27 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Game extends Application {
-
-    public enum STATE {
-        MENU,
-        PLAYER,
-        COMPUTER,
-        EXIT
-    }
-
-    public static STATE gameState = STATE.MENU;
     private boolean loop = true;
 
     private void menu(Stage stage) {
         stage.setTitle("Menu");
 
-        InputStream stream = null;
-        try {
-            stream = new FileInputStream("res/menu.png");
-        } catch (IOException e) {
-            e.getMessage();
-        }
-
-        Image image = new Image(stream);
-        ImageView imageView = new ImageView(image);
-        imageView.setImage(image);
-
-        Button buttonPlayer = new Button("");
-        buttonPlayer.setLayoutX(BUTTON_PLAYER_START_X);
-        buttonPlayer.setLayoutY(BUTTON_PLAYER_START_Y);
-        buttonPlayer.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-        buttonPlayer.setStyle("-fx-background-color: transparent;");
         buttonPlayer.setOnAction(e -> {
             gameState = STATE.PLAYER;
             gameLoop(stage);
         });
 
-        Button buttonComputer = new Button("");
-        buttonComputer.setLayoutX(BUTTON_COMPUTER_START_X);
-        buttonComputer.setLayoutY(BUTTON_COMPUTER_START_Y);
-        buttonComputer.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-        buttonComputer.setStyle("-fx-background-color: transparent;");
         buttonComputer.setOnAction(e -> {
             gameState = STATE.COMPUTER;
             gameLoop(stage);
         });
 
-        Button buttonExit = new Button("");
-        buttonExit.setLayoutX(BUTTON_EXIT_START_X);
-        buttonExit.setLayoutY(BUTTON_EXIT_START_Y);
-        buttonExit.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-        buttonExit.setStyle("-fx-background-color: transparent;");
         buttonExit.setOnAction(e -> {
             gameState = STATE.EXIT;
             gameLoop(stage);
         });
 
-        Group root = new Group(imageView);
+        Group root = new Group(menuImage);
         root.getChildren().add(buttonPlayer);
         root.getChildren().add(buttonComputer);
         root.getChildren().add(buttonExit);
@@ -91,7 +53,6 @@ public class Game extends Application {
     private void player(Stage stage) {
         stage.setTitle("Player");
 
-        // render red screen
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.setFill(Color.RED);
@@ -121,14 +82,12 @@ public class Game extends Application {
             gameLoop(stage);
         });
 
-
         Group root = new Group(canvas);
         root.getChildren().add(buttonBack);
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
 
     public void gameLoop(Stage stage) {
         switch (gameState) {
@@ -147,8 +106,14 @@ public class Game extends Application {
         }
     }
 
+    private void loadAssets() {
+        loadMenu();
+    }
+
     @Override
     public void start(Stage stage) {
+        loadAssets();
+
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
