ab5029151afcaca7ce81a6630dc0e2cf1f1d0573
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2023-08-20 18:36:25 +0700

    Update: Entity

--- src/object/Entity.java
+++ src/object/Entity.java
@@ -5,13 +5,49 @@ import javafx.scene.canvas.GraphicsContext;
 
 public abstract class Entity {
     private boolean visible;
-    private int x;
-    private int y;
+    private double x;
+    private double y;
 
     public Entity() {
         visible = true;
         x = 0;
         y = 0;
+    }
+
+    public Entity(double x, double y) {
+        visible = true;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Entity(double x, double y, boolean visible) {
+        this.visible = visible;
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
     }
 
     public abstract void render(GraphicsContext gc);
